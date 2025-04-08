bc40e5fa21dc3964b188ebb262806bd1492950cf
toby-tan-ics4u1-02-25 <toby.tan@student.tdsb.on.ca>
2025-04-08 22:56:49 +0000

    rastaman

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,51 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class PracticeProblem {
+    public static void main(String[] args) {
+    }
 
-	public static void main(String args[]) {
+    public static String readFile(String filename) {
+        String rastaman = "";
 
-	}
+        try {
+            FileReader reader = new FileReader(new File(filename));
+            int ch;
 
-	
+            while ((ch = reader.read()) != -1) {
+                rastaman += (char) ch;
+            }
 
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("File could not be read.");
+        }
+
+        return rastaman;
+    }
+
+    public static String backwardsReadFile(String filename) {
+        String rastaman = "";
+
+        try {
+            FileReader reader = new FileReader(new File(filename));
+            int ch;
+
+            while ((ch = reader.read()) != -1) {
+                rastaman += (char) ch;
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("File could not be read.");
+        }
+
+        // Reverse the content manually
+        String reversed = "";
+        for (int i = rastaman.length() - 1; i >= 0; i--) {
+            reversed += rastaman.charAt(i);
+        }
+
+        return reversed;
+    }
 }
